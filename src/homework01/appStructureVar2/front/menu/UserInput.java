1eef2b61bb2cf4d94440839f849cefd648af1d07
Admin <1119321.ab@mail.ru>
2025-03-20 04:23:47 +0100

    2 commit

--- src/homework01/appStructureVar2/front/menu/UserInput.java
+++ src/homework01/appStructureVar2/front/menu/UserInput.java
@@ -3,22 +3,40 @@ package homework01.appStructureVar2.front.menu;
 import java.util.Scanner;
 
 public class UserInput {
+    private Scanner scanner;
 
-    public int userInt(String message){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(message);
-        int userInput = scanner.nextInt();
-        return userInput;
+    public UserInput() {
+        this.scanner = new Scanner(System.in);
     }
 
-    public double userDouble(String message){
-        Scanner scanner = new Scanner(System.in);
+    public int userInt(String message) {
+        int userInput =0;
+//        while (true) {
+            System.out.println(message);
+            try {
+                userInput = scanner.nextInt();
+                if (userInput <= 0) {
+                    System.out.println("Error: Task ID must be a positive number.");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Invalid input. Please enter a valid number.");
+            }return userInput;
+//        }
+    }
+
+    public double userDouble(String message) {
+        double userDouble =0;
         System.out.println(message);
-        double userDouble = scanner.nextDouble();
+        try {
+            userDouble = scanner.nextDouble();
+        }catch (NumberFormatException e){
+            System.out.println("Error: Invalid input. Please enter a valid decimal number.");
+        }
         return userDouble;
     }
 
-    public String userText(String message){
+    public String userText(String message) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(message);
         String userText = scanner.nextLine();
